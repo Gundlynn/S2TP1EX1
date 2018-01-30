1f85794082774259cb9dbe5beed6c190a289835e
Gundy <31841183+GundyBiuh@users.noreply.github.com>
2018-01-30 17:09:26 +0100

    Update TP1ex1.cpp

--- TP1ex1.cpp
+++ TP1ex1.cpp
@@ -18,8 +18,50 @@ void saisie (fraction& a)
 
 void affichage (fraction a)
 {
-	std::cout<<a.num<<"/"<<a.den;
-	std::cout<<std::endl;
+	std::cout << "La fraction est : " << a.num << "/" <<a.den;
+	std::cout << std::endl;
+}
+
+void affichmult (fraction a)
+{
+  std::cout << "La multiplication est : " << a.num << "/" << a.den;
+  std::cout << std::endl;
+}
+
+void affichadd (fraction a)
+{
+  std::cout << "L'addition est : " << a.num << "/" << a.den;
+  std::cout << std::endl;
+}
+
+void affichopp (fraction a)
+{
+  std::cout << "L'opposé est : " << a.num << "/" << a.den;
+  std::cout << std::endl;
+}
+
+void affichinv (fraction a)
+{
+  std::cout << "L'inverse est : " << a.num << "/" << a.den;
+  std::cout << std::endl;
+}
+
+void affichsoustr (fraction a)
+{
+  std::cout << "La soustraction est : " << a.num << "/" << a.den;
+  std::cout << std::endl;
+}
+
+void affichdiv (fraction a)
+{
+  std::cout << "La division est : " << a.num << "/" << a.den;
+  std::cout << std::endl;
+}
+
+void affichpgcd (int a)
+{
+  std::cout << "Le PGCD est : " << a ;
+  std::cout << std::endl;
 }
 
 fraction mult (fraction a,fraction b)
@@ -31,19 +73,77 @@ fraction mult (fraction a,fraction b)
 	//std::cout<<std::endl;
  return res;
 }
-
+ fraction add (fraction a, fraction b) 
+  {
+   fraction res;
+   res.num = ((a.num * b.den)+(b.num * a.den));
+   res.den = a.den * b.den;
+   return res;
+ }
+fraction opp (fraction a)
+  {
+  fraction res;
+  res.num = ( a.num * -1);
+  res.den = ( a.den * -1);
+  return res;
+}
+fraction inv (fraction a)
+  {
+  fraction res;
+  res.num = a.den;
+  res.den = a.num;
+  return res;
+}
+fraction soustr (fraction a, fraction b)
+  {
+  fraction res;
+  res.num = ((a.num * b.den)-(b.num * a.den));
+  res.den = a.den * b.den;
+  return res;
+}
+fraction div (fraction a, fraction b)
+{
+  fraction res;
+  res.num = a.num * b.den;
+  res.den = a.den * b.num;
+  return res;
+}
+int pgcd (int a, int b)
+{
+   if ( a < b ) {
+     int temp = a;
+     a = b ;
+     b = temp ;
+   }
+    while(b!=0){
+      int c=a%b;
+      a=b;
+      b=c;
+    }
+      return a;
+}
 int main()
 { 
  fraction A,B;
  saisie (A);
  saisie (B);
+ int a = 0,b = 0;
+ 
  affichage(A);
  affichage(B);
-  affichage(mult(A,B));
+ affichmult(mult(A,B));
+ affichadd(add(A,B));
+ affichopp(opp(A));
+ affichopp(opp(B));
+ affichinv(inv(A));
+ affichinv(inv(B));
+ affichsoustr(soustr(A,B));
+ affichdiv(div(A,B));
+ 
+ std::cout << "Entrez deux entiers : ";
+ std::cin >> a >> b;
+ std::cout << std::endl;
+ affichpgcd(pgcd(a,b));
 
-	/*std::cout<<"La fraction finale est : "<< <<"/"<< ;
-	std::cin>> ;
-	std::cout<<std::endl;
-*/
 return 0;
 }
